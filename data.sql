497f4e563b596ab2678df915736b7d0fe924d2a2
EmanZHD <zahidimane1@gmail.com>
2024-11-30 19:28:03 +0100

    database tables for sessions and users

---
+++ data.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ username text NOT NULL,
+ email text NOT NULL,
+ passwd text NOT NULL,
+ expired_at TIME DEFAULT TIMESTAMP
+);
+
+CREATE TABLE sessions (
+ id INTEGER Primary Key AUTOINCREMENT,
+ id_user NOT NULL,
+ expired_at TIME DEFAULT TIMESTAMP,
+ created_at TIME DEFAULT TIMESTAMP,
+ FOREIGN KEY (id_user) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
