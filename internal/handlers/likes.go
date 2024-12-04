0bd3a2e905f4fb826eb4a14d38a60a1b64c449b2
fennadaf <fatimaezzahraennadafy@gmail.com>
2024-12-04 19:17:21 +0100

    d

--- internal/handlers/likes.go
+++ internal/handlers/likes.go
@@ -1,12 +1,82 @@
 package handlers
 
 import (
+	"encoding/json"
+	"net/http"
+	"strings"
+
 	"forum/internal/middleware"
 	"forum/internal/services"
+	"forum/internal/utils"
 )
 
 type LikeHandler struct {
 	LikeService   *services.LikeService
 	AuthService   *services.AuthService
 	AuthMidlaware *middleware.AuthMiddleware
+}
+
+func (l *LikeHandler) LikePost(w http.ResponseWriter, r *http.Request) {
+	l.react(w, r, "post", "like")
+}
+
+func (l *LikeHandler) DisLikePost(w http.ResponseWriter, r *http.Request) {
+	l.react(w, r, "post", "dislike")
+}
+
+func (l *LikeHandler) LikeComment(w http.ResponseWriter, r *http.Request) {
+	l.react(w, r, "comment", "like")
+}
+
+func (l *LikeHandler) DisLikeComment(w http.ResponseWriter, r *http.Request) {
+	l.react(w, r, "comment", "dislike")
+}
+
+func (l *LikeHandler) react(w http.ResponseWriter, r *http.Request, liked, typeOf string) {
+
+	if r.Method != http.MethodPost {
+		utils.Error(w, http.StatusMethodNotAllowed)
+		return
+	}
+
+	pathParts := strings.Split(r.URL.Path, "/")
+
+	if len(pathParts) != 3 {
+		utils.Error(w, http.StatusNotFound)
+		return
+	}
+
+	ID := pathParts[2]
+
+	isLogged, usermid := l.AuthMidlaware.IsUserLoggedIn(w, r)
+
+	if isLogged {
+		var err error
+
+		if liked == "post" {
+			err = l.LikeService.Create(usermid.ID, ID, "", typeOf, false)
+		} else {
+			err = l.LikeService.Create(usermid.ID, "", ID, typeOf, true)
+		}
+
+		if err != nil {
+			utils.Error(w, http.StatusInternalServerError)
+			return
+		}
+
+		data, err := l.LikeService.GetLikes(ID, liked)
+		if err != nil {
+			utils.Error(w, http.StatusInternalServerError)
+			return
+		}
+
+		// header to indicate that the content being sent is JSON
+		w.Header().Set("Content-Type", "application/json")
+		// Creates a JSON encoder that writes to the HTTP response writer
+		// Converts the data into a JSON-formatted string
+		// Writes the JSON directly to the HTTP response
+		json.NewEncoder(w).Encode(data)
+	} else {
+		utils.Error(w, http.StatusForbidden)
+	}
 }
--- internal/services/like_service.go
+++ internal/services/like_service.go
@@ -1,9 +1,72 @@
 package services
 
 import (
+	"forum/internal/models"
 	"forum/internal/repositories"
+
+	"github.com/gofrs/uuid/v5"
 )
 
 type LikeService struct {
 	LikeRepo *repositories.LikeReposetorie
 }
+
+func (l *LikeService) GetLikes(ID, liked string) (any, error) {
+	data, err := l.LikeRepo.GetLikes(ID, liked)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
+func (l *LikeService) Create(userID uuid.UUID, postID, commentID, reactType string, isComment bool) error {
+
+	likeID := uuid.Must(uuid.NewV4())
+
+	var postIDPtr, commentIDPtr *string
+
+	if postID != "" {
+		postIDPtr = &postID
+	}
+
+	if commentID != "" {
+		commentIDPtr = &commentID
+	}
+
+	like := &models.Like{
+		ID:        likeID,
+		UserID:    userID,
+		PostID:    postIDPtr,
+		CommentID: commentIDPtr,
+		ReactType: reactType,
+	}
+
+	if isComment {
+		return l.LikeRepo.CreateLike(like, "comment")
+	}
+
+	return l.LikeRepo.CreateLike(like, "post")
+}
+
+// var reaction int
+// pre, err := db.Prepare("SELECT is_like FROM likes WHERE post_id = ? AND user_id = ? ")
+// if err != nil {
+// 	fmt.Println(err.Error())
+// 	return err
+// }
+// defer pre.Close()
+// err = pre.QueryRow(id, id_user).Scan(&reaction)
+// if err != nil {
+// 	fmt.Println(err.Error())
+// 	return err
+// }
+// if _, err := db.Exec(`DELETE FROM Likes WHERE post_id = ? AND user_id = ? `, id, id_user); err != nil {
+// 	fmt.Println(err.Error())
+// 	return err
+// }
+// if reaction == 2 {
+// 	if _, err = db.Exec(`INSERT INTO likes (post_id,user_id,is_like) VALUES (?,?,?)`, id, id_user, 1); err != nil {
+// 		fmt.Println(err.Error())
+// 		return err
+// 	}
+// }
